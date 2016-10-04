42013ca5287d4c15f4989906f76c9008fefac862
Eduardo Vieira <carlos.vieira@pmm.am.gov.br>
2016-10-04 14:34:07 -0400

    Melhoria na consulta

--- www/js/app.js
+++ www/js/app.js
@@ -13,52 +13,73 @@ angular.module('starter', ['ionic','angular-loading-bar'])
 
   $scope.doAlert = function(){
 
+      var semana=new Array(7);
+          semana[0]='Domingo';
+          semana[1]='Segunda';
+          semana[2]='Terça';
+          semana[3]='Quarta';
+          semana[4]='Quinta';
+          semana[5]='Sexta';
+          semana[6]='Sábado';
+
       switch ($scope.form.Mes){
-      case "1":
-        mes = "01";
-        break;
-      case "2":
-        mes = "02";
-        break;
-      case "3":
-        mes = "03";
-        break;
-      case "4":
-        mes = "04";
-        break;
-      case "5":
-        mes = "05";
-        break;
-      case "6":
-        mes = "06";
-        break;
-      case "7":
-        mes = "07";
-        break;
-      case "8":
-        mes = "08";
-        break;
-      case "9":
-        mes = "09";
-        break;
-      case "10":
-        mes = "10";
-        break;
-      case "11":
-        mes = "11";
-        break;
-      case "12":
-        mes = "12";
-        break;
-      default:
-        mes = "0";
-        break;
+        case "1":
+          mes = "01";
+          break;
+        case "2":
+          mes = "02";
+          break;
+        case "3":
+          mes = "03";
+          break;
+        case "4":
+          mes = "04";
+          break;
+        case "5":
+          mes = "05";
+          break;
+        case "6":
+          mes = "06";
+          break;
+        case "7":
+          mes = "07";
+          break;
+        case "8":
+          mes = "08";
+          break;
+        case "9":
+          mes = "09";
+          break;
+        case "10":
+          mes = "10";
+          break;
+        case "11":
+          mes = "11";
+          break;
+        case "12":
+          mes = "12";
+          break;
+        default:
+          mes = "0";
+          break;
     }
-   
+    function getDayString(year, month, dia){
+     return new Date(year, (month-1), dia).getDay();
+    }
+
     $http.get('http://wrsolucoesinformatica.com/server-ponto.php?tx_matricula='+ $scope.form.Matricula +'&tx_mes_periodo='+ mes +'&tx_ano_periodo='+ $scope.form.Ano)
     .then(function (response) {
       if(response.data.OK==true){
         $scope.dados = response.data;
+        $scope.datafild =[];
+        var keys = Object.keys(response.data.retorno);
+        for(var i = 0; i < keys.length ;i++){          
+          var batidas = response.data.retorno[keys[i]];
+          $scope.datafild.push({date: keys[i]+'/'+response.data.mes+'/'+response.data.ano, 
+                                diaString: semana[getDayString(response.data.ano,response.data.mes,keys[i])],
+                                batidas: batidas});
+        }
+               
         $scope.Nome = 'SERVIDOR: '+response.data.nome;
         $scope.Erro ='';
         $scope.listshow =true;
